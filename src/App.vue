dcc947e60d8c602b8c8bc1ea42e7e8693b61ce01
DudchenkoWeb <aa.dudchenko00@gmail.com>
2022-05-07 18:29:31 +0300

    added a title, + git push --forse

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div id="app" class="app">
+    <h1 class="app__title">Filtering cars application</h1>
     <app-search></app-search>
     <app-cars></app-cars>
     
@@ -21,24 +22,42 @@ export default {
 
 </script>
 
-<style>
-@import "../reset.css";
 
-  body {
-    min-width: 360px;
-    min-height: 100vh;
-    overflow-x: hidden;
-    position: relative;
-    font-family: 'Roboto', sans-serif;
-    font-weight: 400;
-    color:#232429
+<style lang="scss">
+  @import "../reset.css";
+
+  .app {
+    padding: 50px 0 30px;
+
+    &__title {
+      text-align: center;
+      font-size: 26px;
+      color:cadetblue
+    }
   }
 
-  .my-container {
-    max-width: 1480px; 
-    padding: 0 20px;
-    margin: 0 auto;
-  }  
+</style>
 
+
+<style>
+
+  body {
+      min-width: 360px;
+      min-height: 100vh;
+      overflow-x: hidden;
+      position: relative;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 400;
+      color:#232429
+    }
+
+    .my-container {
+      max-width: 1480px; 
+      padding: 0 20px;
+      margin: 0 auto;
+    }  
 
 </style>
+
+
+
